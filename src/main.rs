00380cf97ec5fbc1b8ca842956824f3ce2345ffd
Faiyaz <faiyazismail111@gmail.com>
2026-06-13 23:16:58 +0600

    added cors

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,11 @@ async fn main() {
 
     // cors setup for frontend
     let cors = CorsLayer::new()
-        .allow_origin(Any)
+        .allow_origin([
+            "http://localhost:5173".parse::<http::HeaderValue>().unwrap(),
+            "http://localhost:4173".parse::<http::HeaderValue>().unwrap(),
+            "https://cp-geeks-frontend.vercel.app".parse::<http::HeaderValue>().unwrap(),
+        ])
         .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
         .allow_headers(Any);
 
